2ca4f064f5958e33195ef5f9b8bffd93cf97c152
Aditya Sharma <sharma.aditya9164@gmail.com>
2024-01-26 19:16:14 +0530

    starting the project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/Signin'
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
+export default function App() {
+  return <BrowserRouter>
+  <Header />
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/signin" element={<SignIn />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+  </BrowserRouter>
+}
+
+
